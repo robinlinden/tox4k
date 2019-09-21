25789fde9d25ca6ab2915a98401db58f36138063
Robin Lindén <dev@robinlinden.eu>
2019-09-21 16:06:54 +0200

    Clean up CMakeLists.txt

--- app/src/main/java/ltd/evilcorp/tox4k/MainActivity.kt
+++ app/src/main/java/ltd/evilcorp/tox4k/MainActivity.kt
@@ -24,7 +24,7 @@ class MainActivity : AppCompatActivity() {
 
         // Used to load the 'native-lib' library on application startup.
         init {
-            System.loadLibrary("native-lib")
+            System.loadLibrary("tox4k")
         }
     }
 }
