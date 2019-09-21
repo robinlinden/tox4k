5f666ea2f16e0466b0346e7c798ee4ca3adacc15
Robin Lindén <dev@robinlinden.eu>
2019-09-21 16:31:45 +0200

    Add libsodium to the C++ build

--- app/src/main/cpp/native-lib.cpp
+++ app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,13 @@
 #include <jni.h>
+#include <sodium.h>
+
 #include <string>
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_ltd_evilcorp_tox4k_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = "Hello from C++\n";
+    hello.append(std::to_string(randombytes_random()));
     return env->NewStringUTF(hello.c_str());
 }
